cc57235d3721f5f1447ca8ea2088ab5cdf3ef662
Sanket <2016bcs016@sggs.ac.in>
2020-01-18 14:09:44 +0530

    Implemented bubble & selection sort separately (!generic)

--- js/index.js
+++ js/index.js
@@ -1,12 +1,173 @@
-let array = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
-// let bubbleSorter = new InsertionSorter();
+let array = [15, 8, 11, 17, 13, 5, 6, 4, 10];
+// let array = ['a', 'e', 'f', 'b', 'c', 'd', 'g', 'h', 'i'];
 
-// bubbleSorter.sort(array);
+console.log(array)
 
-let selectionSorter = new InsertionSorter();
+let bubbleMoves = Sorter.sort(array, Sorter.type.bubble);
 
-selectionSorter.sort(array);
-//
-// let insertionSorter = new InsertionSorter();
-//
-// insertionSorter.sort(array);
+// window.getComputedStyle()
+let maxWidth = parseInt(window.getComputedStyle(document.getElementById("sorter"), null).getPropertyValue("width"));
+let maxHeight = parseInt(window.getComputedStyle(document.getElementById("sorter"), null).getPropertyValue("height"));
+let viewMap = Sorter.createViews(array, maxWidth, maxHeight);
+Sorter.layoutViews("sorter", viewMap, array.length);
+
+animate(array, Sorter.type.bubble);
+
+function onSliderChange() {
+    console.log("changed")
+    for (let i = 0; i < array.length; i++) {
+        viewMap[i].style.transitionDuration = document.getElementById('slider').value + 's';
+    }
+}
+
+function animate(array, type) {
+    switch (type) {
+        case Sorter.type.bubble:
+            bubbleSortAnimate(array);
+            break;
+        case Sorter.type.insertion:
+
+            break;
+        case Sorter.type.selection:
+            selectionSortAnimate(array);
+            break;
+        case Sorter.type.merge:
+
+            break;
+        case Sorter.type.quick:
+
+            break;
+    }
+}
+
+function selectionSortAnimate(array) {
+    let index = 0;
+    let first = -1;
+    let second = -1;
+    let minIndex = -1;
+
+    function animateInternally() {
+        if (first !== -1) {
+            viewMap[first].classList.remove('active');
+            viewMap[first].classList.remove('moving');
+        }
+
+        if (second !== -1) {
+            viewMap[second].classList.remove('active');
+            viewMap[second].classList.remove('moving');
+        }
+
+        let move = bubbleMoves[index];
+
+        switch (move.type) {
+            case Sorter.moveType.next:
+                first = move.first;
+                second = move.second;
+                viewMap[first].classList.add('active');
+                viewMap[second].classList.add('active');
+                break;
+            case Sorter.moveType.swap:
+                first = move.first;
+                second = move.second;
+                viewMap[first].classList.add('active');
+                viewMap[first].classList.add('moving');
+                viewMap[minIndex].classList.add('moving');
+
+                let fx = viewMap[first].style.left;
+                let sx = viewMap[second].style.left;
+
+                viewMap[first].style.left = sx;
+                viewMap[second].style.left = fx;
+
+                let t = viewMap[first];
+                viewMap[first] = viewMap[second];
+                viewMap[second] = t;
+
+                break;
+            case Sorter.moveType.minimum:
+                if (minIndex !== -1) {
+                    viewMap[minIndex].classList.remove('minimum');
+                }
+                viewMap[first].classList.add('active');
+                viewMap[second].classList.add('active');
+                first = move.first;
+                second = move.second;
+                minIndex = first;
+                viewMap[first].classList.add('minimum');
+                break;
+        }
+
+        index++;
+
+        if (index < bubbleMoves.length) {
+            setTimeout(animateInternally, document.getElementById("slider").value * 1000);
+        } else {
+            viewMap[first].classList.remove('active');
+            viewMap[first].classList.remove('moving');
+            viewMap[second].classList.remove('active');
+            viewMap[second].classList.remove('moving');
+            viewMap[second].classList.add('minimum');
+            viewMap[array.length - 1].classList.add('minimum');
+        }
+    }
+
+    animateInternally();
+}
+
+function bubbleSortAnimate(array) {
+    let index = 0;
+    let first = -1;
+    let second = -1;
+
+    function animateInternally() {
+        if (first !== -1 && second !== -1) {
+            viewMap[first].classList.remove('active');
+            viewMap[first].classList.remove('moving');
+            viewMap[second].classList.remove('active');
+            viewMap[second].classList.remove('moving');
+        }
+        let move = bubbleMoves[index];
+
+        switch (move.type) {
+            case Sorter.moveType.next:
+                first = move.first;
+                second = move.second;
+                viewMap[first].classList.add('active');
+                viewMap[second].classList.add('active');
+                break;
+            case Sorter.moveType.swap:
+                first = move.first;
+                second = move.second;
+                viewMap[first].classList.add('active');
+                viewMap[second].classList.add('active');
+                viewMap[first].classList.add('moving');
+                viewMap[second].classList.add('moving');
+
+                let fx = viewMap[first].style.left;
+                let sx = viewMap[second].style.left;
+
+                viewMap[first].style.left = sx;
+                viewMap[second].style.left = fx;
+
+                let t = viewMap[first];
+                viewMap[first] = viewMap[second];
+                viewMap[second] = t;
+
+                break;
+        }
+
+        index++;
+
+        if (index < bubbleMoves.length) {
+            setTimeout(animateInternally, document.getElementById("slider").value * 1000);
+        } else {
+            viewMap[first].classList.remove('active');
+            viewMap[first].classList.remove('moving');
+            viewMap[second].classList.remove('active');
+            viewMap[second].classList.remove('moving');
+        }
+    }
+
+    animateInternally();
+}
+
--- js/sorter.base.js
+++ js/sorter.base.js
@@ -1,63 +1,238 @@
 class Sorter {
-    /*
-    props {
-        requiresPivot: boolean
-    }
-     */
+    static type = {
+        merge: 1,
+        bubble: 2,
+        selection: 3,
+        insertion: 4,
+        quick: 5
+    };
 
-    constructor(props) {
-        this.array = [];
-        this.pivot = -1;
-        this.first = -1;
-        this.second = -1;
-        this.minimum = -1;
-        this.temporary = -1;
-        this.needToSwap = false;
-    }
+    static moveType = {
+        next: 1,
+        swap: 2,
+        shift: 3,
+        insert: 4,
+        temporary: 5,
+        select: 6,
+        minimum: 7
+    };
 
-    sort(array) {
-        this.array = array;
-        this.selectInitial();
+    static maxElements = 15;
 
-        while (!this.isSorted()) {
-            // console.log(this.first, this.second);
-            this.compare();
-            if (this.needToSwap) {
-                this.swap();
-            }
-            this.selectNext();
-            // console.log(this.array);
+    static createViews(array, maxWidth, maxHeight) {
+        let total = array.length;
+        let maxSize = 50;
+        let minSize = 30;
+        let maxSpacing = 50;
+        let minSpacing = 30;
+        let spacing = minSpacing + (maxSpacing - minSpacing) * Math.cos((total / Sorter.maxElements) * Math.PI / 2);
+        let size = minSize + (maxSize - minSize) * Math.cos((total / Sorter.maxElements) * Math.PI / 2);
+        let startLeft = (maxWidth - size * total - spacing * (total - 1)) / 2;
+
+        let minValue = Math.min.apply(Math, array);
+        let maxValue = Math.max.apply(Math, array);
+
+        function create(number, index) {
+            let left = parseInt(startLeft + index * (spacing + size));
+            let top = maxHeight - 0.2 * maxHeight - 0.6 * maxHeight * array[index] / maxValue;
+            let height = 0.8 * maxHeight - top;
+            let view = document.createElement("div");
+            view.classList.add('elem');
+            view.style.width = size + 'px';
+            view.style.height = size + 'px';
+            view.style.borderRadius = size / 2 + 'px';
+            view.style.left = left + 'px';
+            view.style.top = top + 'px';
+            view.style.height = height + 'px';
+            view.innerHTML = number;
+            return view;
         }
 
-        this.finish();
-        console.log(this.array);
+        let viewMap = {};
+
+        for (let i = 0; i < array.length; i++) {
+            viewMap[i] = create(array[i], i, array.length);
+        }
+
+        return viewMap;
     }
 
-    swap() {
-        let t = this.array[this.first];
-        this.array[this.first] = this.array[this.minimum !== -1 ? this.minimum : this.second];
-        this.array[this.minimum !== -1 ? this.minimum : this.second] = t;
-        this.minimum = -1;
+    static layoutViews(id, viewMap, total) {
+        for(let i = 0; i < total; i++) {
+            document.getElementById(id).appendChild(viewMap[i]);
+        }
     }
 
-    selectInitial() {
+    static sort(array, type) {
+        let moves = [];
 
+        function bubble(array) {
+            for (let i = 0; i < array.length - 1; i++) {
+                for (let j = i + 1; j < array.length; j++) {
+                    moves.push({
+                        first: i,
+                        second: j,
+                        type: Sorter.moveType.next
+                    });
+                    if (array[i] > array[j]) {
+                        let t = array[i];
+                        array[i] = array[j];
+                        array[j] = t;
+                        moves.push({
+                            first: i,
+                            second: j,
+                            type: Sorter.moveType.swap
+                        });
+                    }
+                }
+            }
+            return moves;
+        }
+
+        function insertion(array) {
+            for (let i = 1; i < array.length; i++) {
+                let t = array[i];
+                let j = i - 1;
+                moves.push({
+                    first: t,
+                    second: -1,
+                    type: Sorter.moveType.temporary
+                });
+                moves.push({
+                    first: j,
+                    second: -1,
+                    type: Sorter.moveType.select
+                });
+                for (; j >= 0; j--) {
+                    if (array[j] < t) {
+                        j++;
+                        break;
+                    }
+                    moves.push({
+                        first: j,
+                        second: -1,
+                        type: Sorter.moveType.shift
+                    });
+                    array[j + 1] = array[j];
+                }
+                if (j >= 0) {
+                    moves.push({
+                        first: j,
+                        second: t,
+                        type: Sorter.moveType.insert
+                    });
+                    array[j] = t;
+                }
+            }
+            return moves;
+        }
+
+        function selection(array) {
+            for (let i = 0; i < array.length - 1; i++) {
+                let k = i;
+                for (let j = i + 1; j < array.length; j++) {
+                    moves.push({
+                        first: i,
+                        second: j,
+                        type: Sorter.moveType.next
+                    });
+                    if (array[j] < array[k]) {
+                        moves.push({
+                            first: j,
+                            second: -1,
+                            type: Sorter.moveType.minimum
+                        });
+                        k = j;
+                    }
+                }
+                let t = array[k];
+                array[k] = array[i];
+                array[i] = t;
+                moves.push({
+                    first: i,
+                    second: k,
+                    type: Sorter.moveType.swap
+                });
+            }
+            return moves;
+        }
+
+        function partition(array, left, right) {
+            let pivot = array[Math.floor((right + left) / 2)];
+            let i = left;
+            let j = right;
+            while (i <= j) {
+                while (array[i] < pivot) {
+                    i++;
+                }
+                while (array[j] > pivot) {
+                    j--;
+                }
+                if (i <= j) {
+                    let t = array[j];
+                    array[j] = array[i];
+                    array[i] = t;
+                    i++;
+                    j--;
+                }
+            }
+            return i;
+        }
+
+        function quick(array, left, right) {
+            if (array.length > 1) {
+                let index = partition(array, left, right);
+                if (left < index - 1) {
+                    Sorter.quick(array, left, index - 1)
+                }
+                if (index < right) {
+                    Sorter.quick(array, index, right)
+                }
+            }
+            return array;
+        }
+
+        function merge(leftArray, rightArray) {
+            let sortedArr = [];
+            while (leftArray.length && rightArray.length) {
+                if (leftArray[0] <= rightArray[0]) {
+                    sortedArr.push(leftArray[0]);
+                    leftArray = leftArray.slice(1)
+                } else {
+                    sortedArr.push(rightArray[0]);
+                    rightArray = rightArray.slice(1)
+                }
+            }
+            while (leftArray.length)
+                sortedArr.push(leftArray.shift());
+            while (rightArray.length)
+                sortedArr.push(rightArray.shift());
+            return sortedArr;
+        }
+
+        function mergeSort(array) {
+            if (array.length < 2) {
+                return array;
+            }
+            let midpoint = parseInt(array.length / 2);
+            let leftArray = array.slice(0, midpoint);
+            let rightArray = array.slice(midpoint, array.length);
+            return merge(mergeSort(leftArray), mergeSort(rightArray));
+        }
+
+        let arrayCopy = array.slice();
+        switch (type) {
+            case 1:
+                return mergeSort(arrayCopy);
+            case 2:
+                return bubble(arrayCopy);
+            case 3:
+                return selection(arrayCopy);
+            case 4:
+                return insertion(arrayCopy);
+            case 5:
+                return quick(arrayCopy);
+        }
     }
-
-    selectNext() {
-
-    }
-
-    isSorted() {
-
-    }
-
-    compare() {
-
-    }
-
-    finish() {
-
-    }
-
 }
+
